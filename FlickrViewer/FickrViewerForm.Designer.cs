35ed9bc69a08a967624c400fab05487ef71f6d22
Артур Фундукян <artur.fundukian@kture.edu.ua>
2019-04-05 23:43:51 -0400

    Second Part is done

---
+++ FlickrViewer/FickrViewerForm.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace FlickrViewer
+{
+   partial class FickrViewerForm
+   {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose( bool disposing )
+      {
+         if ( disposing && ( components != null ) )
+         {
+            components.Dispose();
+         }
+         base.Dispose( disposing );
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent()
+      {
+            this.label1 = new System.Windows.Forms.Label();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.imagesListBox = new System.Windows.Forms.ListBox();
+            this.btnSaveAll = new System.Windows.Forms.Button();
+            this.txtNewSize = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSaveCurrent = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Flickr search tags here:";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.inputTextBox.Location = new System.Drawing.Point(326, 25);
+            this.inputTextBox.Margin = new System.Windows.Forms.Padding(6);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(852, 31);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(326, 127);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(6);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1006, 958);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.searchButton.Location = new System.Drawing.Point(1194, 21);
+            this.searchButton.Margin = new System.Windows.Forms.Padding(6);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(140, 44);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // imagesListBox
+            // 
+            this.imagesListBox.FormattingEnabled = true;
+            this.imagesListBox.ItemHeight = 25;
+            this.imagesListBox.Location = new System.Drawing.Point(30, 127);
+            this.imagesListBox.Margin = new System.Windows.Forms.Padding(6);
+            this.imagesListBox.Name = "imagesListBox";
+            this.imagesListBox.Size = new System.Drawing.Size(280, 954);
+            this.imagesListBox.TabIndex = 5;
+            this.imagesListBox.SelectedIndexChanged += new System.EventHandler(this.imagesListBox_SelectedIndexChanged);
+            // 
+            // btnSaveAll
+            // 
+            this.btnSaveAll.Location = new System.Drawing.Point(326, 72);
+            this.btnSaveAll.Name = "btnSaveAll";
+            this.btnSaveAll.Size = new System.Drawing.Size(280, 46);
+            this.btnSaveAll.TabIndex = 6;
+            this.btnSaveAll.Text = "Save All";
+            this.btnSaveAll.UseVisualStyleBackColor = true;
+            this.btnSaveAll.Click += new System.EventHandler(this.btnSaveAll_ClickAsync);
+            // 
+            // txtNewSize
+            // 
+            this.txtNewSize.Location = new System.Drawing.Point(138, 79);
+            this.txtNewSize.Name = "txtNewSize";
+            this.txtNewSize.Size = new System.Drawing.Size(172, 31);
+            this.txtNewSize.TabIndex = 7;
+            this.txtNewSize.Text = "500";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "New Size";
+            // 
+            // btnSaveCurrent
+            // 
+            this.btnSaveCurrent.Location = new System.Drawing.Point(629, 71);
+            this.btnSaveCurrent.Name = "btnSaveCurrent";
+            this.btnSaveCurrent.Size = new System.Drawing.Size(280, 46);
+            this.btnSaveCurrent.TabIndex = 9;
+            this.btnSaveCurrent.Text = "Save Current Image";
+            this.btnSaveCurrent.UseVisualStyleBackColor = true;
+            this.btnSaveCurrent.Click += new System.EventHandler(this.btnSaveCurrent_ClickAsync);
+            // 
+            // FickrViewerForm
+            // 
+            this.AcceptButton = this.searchButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1358, 1104);
+            this.Controls.Add(this.btnSaveCurrent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNewSize);
+            this.Controls.Add(this.btnSaveAll);
+            this.Controls.Add(this.imagesListBox);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "FickrViewerForm";
+            this.Text = "Flickr Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.TextBox inputTextBox;
+      private System.Windows.Forms.PictureBox pictureBox;
+      private System.Windows.Forms.Button searchButton;
+      private System.Windows.Forms.ListBox imagesListBox;
+        private System.Windows.Forms.Button btnSaveAll;
+        private System.Windows.Forms.TextBox txtNewSize;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSaveCurrent;
+    }
+}
+
---
+++ FlickrViewer/FickrViewerForm.cs
@@ -0,0 +1,221 @@
+﻿// Fig. 23.4: FickrViewerForm.cs
+// Invoking a web service asynchronously with class HttpClient
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace FlickrViewer
+{
+    public partial class FickrViewerForm : Form
+    {
+        // Use your Flickr API key here--you can get one at:
+        // https://www.flickr.com/services/apps/create/apply
+        private const string KEY = "a2b003481632a966c5f43b9671f70a9b";
+
+        // object used to invoke Flickr web service      
+        private static HttpClient flickrClient = new HttpClient();
+
+        Task<string> flickrTask = null; // Task<string> that queries Flickr
+
+        List<FlickrResult> flickrPhotos = new List<FlickrResult>() { };
+
+        public FickrViewerForm()
+        {
+            InitializeComponent();
+        }
+
+        // initiate asynchronous Flickr search query; 
+        // display results when query completes
+        private async void searchButton_Click(object sender, EventArgs e)
+        {
+            // if flickrTask already running, prompt user 
+            if (flickrTask?.Status != TaskStatus.RanToCompletion)
+            {
+                var result = MessageBox.Show(
+                   "Cancel the current Flickr search?",
+                   "Are you sure?", MessageBoxButtons.YesNo,
+                   MessageBoxIcon.Question);
+
+                // determine whether user wants to cancel prior search
+                if (result == DialogResult.No)
+                {
+                    return;
+                }
+                else
+                {
+                    flickrClient.CancelPendingRequests(); // cancel search
+                }
+            }
+
+            // Flickr's web service URL for searches                         
+            var flickrURL = "https://api.flickr.com/services/rest/?method=" +
+               $"flickr.photos.search&api_key={KEY}&" +
+               $"tags={inputTextBox.Text.Replace(" ", ",")}" +
+               "&tag_mode=all&per_page=500&privacy_filter=1";
+
+            imagesListBox.DataSource = null; // remove prior data source
+            imagesListBox.Items.Clear(); // clear imagesListBox
+            pictureBox.Image = null; // clear pictureBox
+            imagesListBox.Items.Add("Loading..."); // display Loading...
+
+            // invoke Flickr web service to search Flick with user's tags
+            flickrTask = flickrClient.GetStringAsync(flickrURL);
+
+            // await flickrTask then parse results with XDocument and LINQ
+            XDocument flickrXML = XDocument.Parse(await flickrTask);
+
+            // gather information on all photos
+            //List<FlickrResult> flickrPhotos = new List<FlickrResult>() { };
+            flickrPhotos =
+            (from photo in flickrXML.Descendants("photo")
+             let id = photo.Attribute("id").Value
+             let title = photo.Attribute("title").Value
+             let secret = photo.Attribute("secret").Value
+             let server = photo.Attribute("server").Value
+             let farm = photo.Attribute("farm").Value
+             select new FlickrResult
+             {
+                 Title = title,
+                 URL = $"https://farm{farm}.staticflickr.com/" +
+                   $"{server}/{id}_{secret}.jpg"
+             }).ToList();
+            imagesListBox.Items.Clear(); // clear imagesListBox
+
+            // set ListBox properties only if results were found
+            if (flickrPhotos.Any())
+            {
+                imagesListBox.DataSource = flickrPhotos.ToList();
+                imagesListBox.DisplayMember = "Title";
+            }
+            else // no matches were found
+            {
+                imagesListBox.Items.Add("No matches");
+            }
+        }
+
+        // display selected image
+        private async void imagesListBox_SelectedIndexChanged(
+           object sender, EventArgs e)
+        {
+            if (imagesListBox.SelectedItem != null)
+            {
+                string selectedURL = ((FlickrResult)imagesListBox.SelectedItem).URL;
+
+                // use HttpClient to get selected image's bytes asynchronously
+                byte[] imageBytes = await flickrClient.GetByteArrayAsync(selectedURL);
+
+                // display downloaded image in pictureBox                  
+                using (var memoryStream = new MemoryStream(imageBytes))
+                {
+                    pictureBox.Image = Image.FromStream(memoryStream);
+                }
+            }
+        }
+
+        // ------------------Added Code------------------
+
+        // All images are saved in FlikrViewer/bin/Debug/Images
+        private static async void ResizeImage(FlickrResult photo, int width)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    var webClient = new WebClient();
+                    byte[] imageBytes = webClient.DownloadData(photo.URL);
+
+                    using (var stream = new MemoryStream(imageBytes))
+                    {
+                        var image = Image.FromStream(stream);
+
+                        var height = (width * image.Height) / image.Width;
+                        var thumbnail = image.GetThumbnailImage(width, height, null, IntPtr.Zero);
+
+                        if (photo.Title != "" && photo.Title != null && photo.Title.Count() <= 250)
+                        {
+                            thumbnail.Save(@"Images\" + photo.Title + ".jpeg");
+                        }
+                    }
+                }
+                catch { }
+                //catch (Exception exception)
+                //{
+                //    MessageBox.Show(exception.Message);
+                //}
+            });
+        }
+        private async void btnSaveAll_ClickAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                int width = Convert.ToInt32(txtNewSize.Text);
+                if (width >= 1 && width <= 2000)
+                {
+                    await Task.Factory.StartNew(() => {
+                        ParallelLoopResult loopResult = Parallel.ForEach<FlickrResult>(flickrPhotos, p =>
+                        {
+                            ResizeImage(p, width);
+                        });
+                    });
+                }
+                else
+                {
+                    MessageBox.Show("Size of an image should be in range 1 to 2000");
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        private void btnSaveCurrent_ClickAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                int width = Convert.ToInt32(txtNewSize.Text);
+                if (width >= 1 && width <= 2000)
+                {
+                    if (imagesListBox.SelectedItem != null)
+                    {
+                        FlickrResult selectedImage = (FlickrResult)imagesListBox.SelectedItem;
+
+                        ResizeImage(selectedImage, width);
+                        MessageBox.Show("The image is saved");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Size of an image should be in range 1 to 2000");
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
+
+/**************************************************************************
+ * (C) Copyright 1992-2017 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
