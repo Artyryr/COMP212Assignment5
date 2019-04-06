0e87148445ecdb66d78a386a22716b6ce402764b
Артур Фундукян <artur.fundukian@kture.edu.ua>
2019-04-05 20:03:51 -0400

    Part 1 is Done.

--- Question 1/MainWindow.xaml.cs
+++ Question 1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Question_1
 {
@@ -23,119 +25,62 @@ namespace Question_1
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static List<Stock> stocks = new List<Stock>() { };
-        private List<Stock> searchResult = new List<Stock>() { };
+        public static ObservableCollection<Stock> stocks = new ObservableCollection<Stock>() { };
+        private ObservableCollection<Stock> searchResult = new ObservableCollection<Stock>() { };
         public static ProgressBar progressBar = new ProgressBar();
         public MainWindow()
         {
             InitializeComponent();
 
-            prBar = progressBar;
-
-            //var result = new Task<List<Stock>>(SearchList, new Tuple<String>("stockData.csv"));
-            //result.Start();
-            //stocks = result.Result;
-
-            stocks = LoadData("stockData.csv");
-            dgrdStock.ItemsSource = stocks;
+            progressBar = prBar;
         }
 
-        //private static List<Stock> SearchList(object filePath)
-        //{
-        //    Tuple<string> path = (Tuple<String>)filePath;
-
-        //    List<Stock> fullList = new List<Stock>() { };
-        //    //try
-        //    //{
-        //        float step = 100F / File.ReadAllLines(path.Item1).Count();
-        //        var reader = new StreamReader(File.OpenRead(path.Item1));
-
-
-        //        var columnNames = reader.ReadLine();
-
-        //        while (!reader.EndOfStream)
-        //        {
-        //            //progressBar.Value += step;
-        //            var line = reader.ReadLine();
-
-        //            if (!line.Contains("("))
-        //            {
-        //                Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-        //                string[] stockElements = CSVParser.Split(line);
-
-        //                for (int i = 0; i < stockElements.Count(); i++)
-        //                {
-        //                    string item = stockElements[i].Replace('$', ' ');
-        //                    item = item.Replace('\\', ' ');
-        //                    item = item.Replace('"', ' ');
-        //                    stockElements[i] = item;
-        //                }
-
-        //                string name = stockElements[0];
-        //                DateTime date = Convert.ToDateTime(stockElements[1]);
-        //                double open = Convert.ToDouble(stockElements[2]);
-        //                double high = Convert.ToDouble(stockElements[3]);
-        //                double low = Convert.ToDouble(stockElements[4]);
-        //                double close = Convert.ToDouble(stockElements[5]);
-
-        //                Stock stock = new Stock(name, date, open, high, low, close);
-        //                fullList.Add(stock);
-        //            }
-        //        }
-        //    //}
-        //    //catch (Exception ex)
-        //    //{
-        //    //    MessageBox.Show("Something is wrong. Try agani...\nException message: " + ex.Message);
-        //    //}
-        //    return fullList;
-        //}
-        //public List<Stock> Search(string searchCriteria)
-        //{
-        //    searchResult = (from stock in stocks
-        //                   where stock.StockName.ToLower().Contains(searchCriteria.ToLower())
-        //                   orderby stock.Date
-        //                   select stock).ToList();
-        //    return searchResult;
-        //}
-
-        public static List<Stock> LoadData(string filePath)
+        public async Task<ObservableCollection<Stock>> LoadData(string filePath)
         {
-            List<Stock> fullList = new List<Stock>() { };
+            ObservableCollection<Stock> fullList = new ObservableCollection<Stock>() { };
             try
             {
                 float step = 100F / File.ReadAllLines(filePath).Count();
                 var reader = new StreamReader(File.OpenRead(filePath));
-                
+
                 var columnNames = reader.ReadLine();
 
                 while (!reader.EndOfStream)
                 {
-                    progressBar.Value += step;
-                    var line = reader.ReadLine();
-
-                    if (!line.Contains("("))
+                    await Task.Factory.StartNew(() =>
                     {
-                        Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-                        string[] stockElements = CSVParser.Split(line);
+                        var line = reader.ReadLine();
+                        Application.Current.Dispatcher.BeginInvoke(
+                            DispatcherPriority.Normal,
+                            new Action(() =>
+                            {
+                                progressBar.Value += step;
+                            }));
 
-                        for (int i = 0; i < stockElements.Count(); i++)
+                        if (!line.Contains("("))
                         {
-                            string item = stockElements[i].Replace('$', ' ');
-                            item = item.Replace('\\', ' ');
-                            item = item.Replace('"', ' ');
-                            stockElements[i] = item;
+                            Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
+                            string[] stockElements = CSVParser.Split(line);
+
+                            for (int i = 0; i < stockElements.Count(); i++)
+                            {
+                                string item = stockElements[i].Replace('$', ' ');
+                                item = item.Replace('\\', ' ');
+                                item = item.Replace('"', ' ');
+                                stockElements[i] = item;
+                            }
+
+                            string name = stockElements[0];
+                            DateTime date = Convert.ToDateTime(stockElements[1]);
+                            double open = Convert.ToDouble(stockElements[2]);
+                            double high = Convert.ToDouble(stockElements[3]);
+                            double low = Convert.ToDouble(stockElements[4]);
+                            double close = Convert.ToDouble(stockElements[5]);
+
+                            Stock stock = new Stock(name, date, open, high, low, close);
+                            fullList.Add(stock);
                         }
-
-                        string name = stockElements[0];
-                        DateTime date = Convert.ToDateTime(stockElements[1]);
-                        double open = Convert.ToDouble(stockElements[2]);
-                        double high = Convert.ToDouble(stockElements[3]);
-                        double low = Convert.ToDouble(stockElements[4]);
-                        double close = Convert.ToDouble(stockElements[5]);
-
-                        Stock stock = new Stock(name, date, open, high, low, close);
-                        fullList.Add(stock);
-                    }
+                    });
                 }
             }
             catch (Exception ex)
@@ -144,20 +89,59 @@ namespace Question_1
             }
             return fullList;
         }
-        private void BtnSearch_Click(object sender, RoutedEventArgs e)
+
+        public ObservableCollection<Stock> Search(string searchCriteria)
+        {
+            searchResult = new ObservableCollection<Stock>((from stock in stocks
+                            where stock.StockName.ToLower().Contains(searchCriteria.ToLower())
+                            orderby stock.Date
+                            select stock).ToList());
+            return searchResult;
+        }
+
+        public static ulong Factorial(ulong number)
+        {
+            ulong result = 1;
+            for(ulong i = 1; i <= number; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }
+
+        private async void BtnSearch_Click(object sender, RoutedEventArgs e)
         {
             if (txtSearch.Text != null && txtSearch.Text != "")
             {
                 string searchCriteria = txtSearch.Text;
-                searchResult = (from stock in stocks
-                                where stock.StockName.ToLower().Contains(searchCriteria.ToLower())
-                                select stock).ToList();
-                dgrdStock.ItemsSource = null;
-                dgrdStock.ItemsSource = searchResult;
+                Task<ObservableCollection<Stock>> resultList = Task.Run(() => Search(searchCriteria));
+                await resultList;
+
+                dgrdStock.ItemsSource = resultList.Result.ToList();
             }
             else
             {
                 dgrdStock.ItemsSource = stocks;
+            }
+        }
+
+        private async void BtnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            Task<ObservableCollection<Stock>> stockList = Task.Run(() => LoadData("stockData.csv"));
+            await stockList;
+            stocks = stockList.Result;
+            dgrdStock.ItemsSource = stocks;
+        }
+
+        private void BtnCalculateFactorial_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                txtFactorialResult.Text = Factorial(Convert.ToUInt64(txtFactorialNum.Text)).ToString();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Something is wrong. Try agani...\nException message: " + ex.Message);
             }
         }
     }
